e8343d9c03952e263e3d28d733baae267f6d3de4
Nirinis <nikita.korsikov1@yandex.ru>
2026-02-20 22:50:16 +0300

    Microfixes in solution's files

---
+++ MSLL/MSLL.cpp
@@ -0,0 +1,10 @@
+﻿// MSLL.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])    //argc - count of args, argv - array of arg-strings
+{
+    std::cout << "Hello World!\n";
+}
+
